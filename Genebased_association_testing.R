693ec98fad72daa362a01497d00c85aa666388aa
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2021-12-23 18:46:20 +0100

    Update Genebased_association_testing.R

--- Genebased_association_testing.R
+++ Genebased_association_testing.R
@@ -141,7 +141,10 @@ perform_burden_collapse <-function(gdsfile, groupfile, phenfile, ID_col, nullfil
 
 # Kernell based gene-based tests			  
 kernell_variance_component <- function(gdsfile, groupfile, phenfile, ID_col, nullfile, outfile,
-				       AF.max=0.001, MAC.max=Inf, use.weights=FALSE, vc.test=c("Score", "Score.SPA"), test=c("SKAT", "SKATO", "SMMAT", "SKAT_SAIGEGENEplus", "Extract"), SAIGEGENEplus_collapse_threshold=10,weight.beta=c(1,1))){
+				       AF.max=0.001, MAC.max=Inf, use.weights=FALSE, 
+				       vc.test=c("Score", "Score.SPA"), 
+				       test=c("SKAT", "SKATO", "SMMAT", "SKAT_SAIGEGENEplus", "ExtractKernelStatistics"), 
+				       SAIGEGENEplus_collapse_threshold=10, weight.beta=c(1,1))){
 	#' 
 	#' gdsfile = string specifying the file name of the genetic dataset; dataset should be in SeqArray GDS format 
 	#' groupfile = string specifyinng the file name of the grouping file; the grouping file contains information of variants to be included in the analysis:
