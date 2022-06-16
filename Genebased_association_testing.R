d144073420ebec6eb2ae875193beb5862f4ef664
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2022-06-16 19:39:16 +0200

    Fix ID issue

--- Genebased_association_testing.R
+++ Genebased_association_testing.R
@@ -100,7 +100,8 @@ perform_burden_collapse <-function(gdsfile, groupfile, phenfile, ID_col, nullfil
 	#if(id_int){class(combphen2$sample.id) <- 'integer'}
 
 	# Construct a SeqVarData object
-	seqData <- SeqVarData(gds, sampleData=AnnotatedDataFrame(combphen2))
+	seq_ids <- seqGetData(gdsfile, var.name='sample.id')
+	class(combphen2$sample.id) <- class(seq_ids)
 
 	# Filter the gdsfile
 	seqSetFilter(seqData, sample.id=samid0)
@@ -237,9 +238,11 @@ kernell_variance_component <- function(gdsfile, groupfile, phenfile, ID_col, nul
 	rownames(combphen)<-combphen$sample.id
 	combphen2<-combphen[samples,]
 	#if(id_int){class(combphen2$sample.id) <- 'integer'}
-	
+
 	# Construct a SeqVarData object
 	seqData <- SeqVarData(gds, sampleData=AnnotatedDataFrame(combphen2))
+	seq_ids <- seqGetData(gdsfile, var.name='sample.id')
+	class(combphen2$sample.id) <- class(seq_ids)
 	
 	# Filter the gdsfile
 	seqSetFilter(seqData, sample.id=samid0)
