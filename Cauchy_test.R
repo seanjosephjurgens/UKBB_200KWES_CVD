21aee679c8185a60269ae18ae29bbcdefee854b6
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2022-06-21 12:19:07 +0200

    Create Cauchy_test.R

---
+++ Cauchy_test.R
@@ -0,0 +1,60 @@
+#! Rscript
+
+############################################################
+# Cauchy distribution test, adapted from the STAAR authors!!
+############################################################
+
+## Cauchy functions
+CCT <- function(pvals, weights=NULL){
+  #### check if there is NA
+  if(sum(is.na(pvals)) > 0){
+    pvals <- pvals[-(which(is.na(pvals))), ]
+  }
+
+  #### check if all p-values are between 0 and 1
+  if((sum(pvals<0) + sum(pvals>1)) > 0){
+    stop("All p-values must be between 0 and 1!")
+  }
+
+  #### check if there are p-values that are either exactly 0 or 1.
+  is.zero <- (sum(pvals==0)>=1)
+  is.one <- (sum(pvals==1)>=1)
+  if(is.zero && is.one){
+    stop("Cannot have both 0 and 1 p-values!")
+  }
+  if(is.zero){
+    return(0)
+  }
+  if(is.one){
+    warning("There are p-values that are exactly 1!")
+    return(1)
+  }
+
+  #### check the validity of weights (default: equal weights) and standardize them.
+  if(is.null(weights)){
+    weights <- rep(1/length(pvals),length(pvals))
+  }else if(length(weights)!=length(pvals)){
+    stop("The length of weights should be the same as that of the p-values!")
+  }else if(sum(weights < 0) > 0){
+    stop("All the weights must be positive!")
+  }else{
+    weights <- weights/sum(weights)
+  }
+
+  #### check if there are very small non-zero p-values
+  is.small <- (pvals < 1e-16)
+  if (sum(is.small) == 0){
+    cct.stat <- sum(weights*tan((0.5-pvals)*pi))
+  }else{
+    cct.stat <- sum((weights[is.small]/pvals[is.small])/pi)
+    cct.stat <- cct.stat + sum(weights[!is.small]*tan((0.5-pvals[!is.small])*pi))
+  }
+
+  #### check if the test statistic is very large.
+  if(cct.stat > 1e+15){
+    pval <- (1/cct.stat)/pi
+  }else{
+    pval <- 1-pcauchy(cct.stat)
+  }
+  return(pval)
+}
